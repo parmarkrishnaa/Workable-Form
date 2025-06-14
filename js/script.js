c2e1af730176c97d0690a1fbe02e0cea4b957f97
parmarkrishnaa <parmarkrishna050611@gmail.com>
2025-06-14 23:43:10 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,14 @@
+// var form = document.getElementById('surveyForm');
+// form.addEventListener("submit", e => {
+//     e.preventDefault();
+//     fetch(form.action, {
+//         method: "POST",
+//         body: new FormData(document.getElementById("surveyForm")),
+//     }).then(
+//         response => response.json()
+//     ).then((_html) => {
+//         alert('Submitted !! 😊 Thank you ' + document.getElementById('firstname').value + ' ' + document.getElementById('lastname').value)
+//         window.location.href = '';
+//         location.reload();
+//     });
+// });
